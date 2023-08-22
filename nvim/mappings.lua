b396b0c8ffce583caf544da7fbb6bda13512fd8c
Luis Vega <377215+luisvega@users.noreply.github.com>
2023-08-21 17:21:00 -0700

    Create mappings.lua

---
+++ nvim/mappings.lua
@@ -0,0 +1,63 @@
+local M = {}
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line"
+    },
+    ["<leader>dus"] = {
+      function ()
+        local widgets = require('dap.ui.widgets');
+        local sidebar = widgets.sidebar(widgets.scopes);
+        sidebar.open();
+      end,
+      "Open debugging sidebar"
+    }
+  }
+}
+
+M.dap_go = {
+  plugin = true,
+  n = {
+    ["<leader>dgt"] = {
+      function()
+        require('dap-go').debug_test()
+      end,
+      "Debug go test"
+    },
+    ["<leader>dgl"] = {
+      function()
+        require('dap-go').debug_last()
+      end,
+      "Debug last go test"
+    }
+  }
+}
+
+M.gopher = {
+  plugin = true,
+  n = {
+    ["<leader>gsj"] = {
+      "<cmd> GoTagAdd json <CR>",
+      "Add json struct tags"
+    },
+    ["<leader>gsy"] = {
+      "<cmd> GoTagAdd yaml <CR>",
+      "Add yaml struct tags"
+    }
+  }
+}
+
+M.general = {
+  n = {
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left"},
+    ["<C-l>"] = { "<cmd> TmuxNavigateLeft<CR>", "window right"},
+    ["<C-j>"] = { "<cmd> TmuxNavigateLeft<CR>", "window down"},
+    ["<C-k>"] = { "<cmd> TmuxNavigateLeft<CR>", "window up"},
+    ["<leader>s"] = {":split<CR>", "split window"},
+    ["<leader>vs"] = {":vsplit<CR>", "vsplit window"},
+  }
+}
+return M
